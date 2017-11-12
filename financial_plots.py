31a8f8b326f90547e6558efe1af327916f3d56b0
Siddhartha Datta <siddharthadtt1@gmail.com>
2017-11-12 23:25:39 +0530

    Added financial plots data

--- financial_plots.py
+++ financial_plots.py
@@ -6,11 +6,39 @@ Created on Sat Nov 11 00:51:49 2017
 """
 
 import pandas_datareader.data as web
+import numpy as np
+import pandas as pd
 
 
-DAX = web.DataReader(name='HUL', data_source='yahoo', start='2000-1-1')
-print DAX.info()
+AAPL = web.DataReader(name='AAPL', data_source='yahoo', start='2005-1-1')
+#AAPL['Close'].plot(figsize=(8,5), style='r', lw=2)
 
-print DAX
+AAPL['Ret_Loop'] = 0.0
+for i in range(1, len(AAPL)):
+    AAPL['Ret_Loop'][i] = np.log(AAPL['Close'][i]/AAPL['Close'][i-1])
 
-DAX['Close'].plot(figsize=(8, 5))
+
+print AAPL[['Close', 'Ret_Loop']].tail()
+
+AAPL['Return'] = np.log(AAPL['Close'] / AAPL['Close'].shift(1))
+
+#print AAPL[['Close', 'Ret_Loop', 'Return']].tail()
+
+# delete AAPL['Ret_Loop'] column 
+del AAPL['Ret_Loop']
+
+#AAPL[['Close', 'Return']].plot(subplots = True, style='b', figsize=(8,5))
+
+AAPL['42d'] = pd.rolling_mean(AAPL['Close'], window=42)
+AAPL['252d'] = pd.rolling_mean(AAPL['Close'], window=252)
+
+#AAPL[['Close', '42d', '252d']].plot(figsize=(8,5))
+AAPL[['Close', '42d', '252d']].plot(subplots=True, style='b', figsize=(8, 7))
+
+
+
+
+
+
+
+
